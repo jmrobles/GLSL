a79577cfcb9675b85713364a917a03ff42100a67
JMRH <jmroblesh@digitalilusion.com>
2013-07-15 19:34:14 +0200

    Initial Commit

---
+++ GLSLTest/TESTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TESTAppDelegate.h
+//  GLSLTest
+//
+//  Created by JM Robles on 15/07/13.
+//  Copyright (c) 2013 JM Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TESTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLSLTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLSLTest
+//
+//  Created by JM Robles on 15/07/13.
+//  Copyright (c) 2013 JM Robles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TESTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TESTAppDelegate class]));
+    }
+}
